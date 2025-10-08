0703a5d3095ff79b5c2875ef65adcd83d4a21697
MeiyanoSei <potatokaykes@gmail.com>
2025-10-09 00:32:24 +0800

    added holder and slider for content

---
+++ script.js
@@ -0,0 +1,14 @@
+new Swiper('.card-wrapper', {
+    loop: true,
+    spaceBetween: 50,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    navigation:{
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
